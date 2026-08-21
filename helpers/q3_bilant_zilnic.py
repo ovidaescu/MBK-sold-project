39f8c164820f93405be0de36a46e85462a0569ca
ovi_daescu <ovi.daescu8@gmail.com>
2026-08-21 13:01:14 +0300

    first changes to q3 graph done

--- helpers/q3_bilant_zilnic.py
+++ helpers/q3_bilant_zilnic.py
@@ -4,28 +4,49 @@ import plotly.express as px
 # ÎNTREBAREA 3: Ore pe zi Import vs. Export
 def print_q3(df):
     st.subheader("3. Număr de ore/zi: Net Importator vs. Net Exportator")
+    # create a new dataframe set the date as index for easier resampling, resample to hourly frequency and calculate the mean sold for each hour
+    # resampling data in groups by hour ( 8:30,8:45 - > group 8)
+    # compute the mean for each hour, reset the index to have a flat dataframe again
     df_hourly = df.set_index("timestamp").resample("1h")[["sold"]].mean().reset_index()
+    # create a new column with only the date from the timpestamp withouth the hour
     df_hourly["data"] = df_hourly["timestamp"].dt.date
+    # new column for status  
     df_hourly["status"] = df_hourly["sold"].apply(
         lambda v: "Import" if v > 0 else "Export" if v < 0 else "Echilibru"
     )
 
     daily_hours = (
-        df_hourly.groupby(["data", "status"])
-        .size()
-        .unstack(fill_value=0)
-        .reset_index()
+        df_hourly.groupby(["data", "status"]) # group by date and status
+        .size() # count the number of occurrences for each group
+        .unstack(fill_value=0) # unstack - create a new dataframe with the status as columns and fill missing values with 0
+        .reset_index() # reset the index to have a flat dataframe again
     )
-    for col in ["Import", "Export"]:
+    for col in ["Import", "Export", "Echilibru"]:
         if col not in daily_hours.columns:
             daily_hours[col] = 0
 
     fig_hours = px.bar(
         daily_hours,
         x="data",
-        y=["Import", "Export"],
+        y=["Import", "Export", "Echilibru"],
         labels={"data": "Data", "value": "Număr ore / zi", "variable": "Regim"},
-        color_discrete_map={"Import": "#d62728", "Export": "#2ca02c"},
+        color_discrete_map={"Import": "#d62728", "Export": "#2ca02c", "Echilibru": "#7f7f7f"},
         title="Bilanțul zilnic al orelor de funcționare",
     )
+
+    # setting for the axis
+    fig_hours.update_layout(
+
+        yaxis=dict(
+            range=[0, 24], 
+            dtick=4, 
+        ),
+        xaxis=dict(
+            dtick="M1",
+            tickformat="%b %Y", 
+            tickangle=0,
+            hoverformat="%d %b %Y", 
+        ),
+    )
+
     st.plotly_chart(fig_hours, use_container_width=True)
